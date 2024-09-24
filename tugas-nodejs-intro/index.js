0dc5017cb79848d7350465281e8a63ed61f99c06
indahmursyida <indahbahrina@gmail.com>
2024-09-24 17:48:29 +0700

    Tugas no 2

--- tugas-nodejs-intro/index.js
+++ tugas-nodejs-intro/index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const http = require('http');
 
 fs.readFile('example.txt', 'utf8', (err, data) => {
   if (err) {
@@ -6,4 +7,15 @@ fs.readFile('example.txt', 'utf8', (err, data) => {
     return;
   }
   console.log(data);
+});
+
+const server = http.createServer((req, res) => {
+  setTimeout(() => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello, World!');
+  }, 1000);
+});
+
+server.listen(3000, () => {
+  console.log('Server running at <http://localhost:3000/>');
 });
