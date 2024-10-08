9efe121bd4eaa25f8876243cc6de33a9302ea319
indahmursyida <indahbahrina@gmail.com>
2024-10-08 10:01:03 +0700

    Added tugas service pattern

---
+++ tugas-service-pattern/src/controllers/categories.controller.ts
@@ -0,0 +1,98 @@
+import { Request, Response } from "express";
+import {
+  create,
+  findAll,
+  findOne,
+  update,
+  remove,
+} from "../services/category.service";
+
+export default {
+  async create(req: Request, res: Response) {
+    try {
+      const result = await create(req.body);
+      res.status(201).json({
+        data: result,
+        message: "Success create category",
+      });
+    } catch (error) {
+      const err = error as Error;
+      res.status(500).json({
+        data: err.message,
+        message: "Failed create category",
+      });
+    }
+  },
+  async findAll(req: Request, res: Response) {
+    try {
+      const search = req.query.search;
+      const page = req.query.page as unknown as number;
+      const limit = req.query.limit as unknown as number;
+
+      const query = {};
+
+      if (search) {
+        Object.assign(query, {
+          name: { $regex: search, $options: "i" },
+        });
+      }
+      const result = await findAll(query, limit, page);
+      res.status(200).json({
+        data: result,
+        message: "Success get all categories",
+      });
+    } catch (error) {
+      const err = error as Error;
+      res.status(500).json({
+        data: err.message,
+        message: "Failed get all categories",
+      });
+    }
+  },
+  async findOne(req: Request, res: Response) {
+    try {
+      const result = await findOne(req.params?.id);
+      res.status(200).json({
+        data: result,
+        message: "Success get one category",
+      });
+    } catch (error) {
+      const err = error as Error;
+      res.status(500).json({
+        data: err.message,
+        message: "Failed get one category",
+      });
+    }
+  },
+  async update(req: Request, res: Response) {
+    try {
+      const result = await update(req.params?.id, req.body);
+
+      res.status(200).json({
+        data: result,
+        message: "Success update category",
+      });
+    } catch (error) {
+      const err = error as Error;
+      res.status(500).json({
+        data: err.message,
+        message: "Failed update category",
+      });
+    }
+  },
+  async delete(req: Request, res: Response) {
+    try {
+      const result = await remove(req.params?.id);
+      res.status(200).json({
+        data: result,
+        message: "Success delete category",
+      });
+    } catch (error) {
+      const err = error as Error;
+      res.status(500).json({
+        data: err.message,
+        message: "Failed delete category",
+      });
+    }
+  },
+};
---
+++ tugas-service-pattern/src/services/category.service.ts
@@ -0,0 +1,37 @@
+import CategoriesModel, { Category } from "../models/categories.model";
+
+export const create = async (payload: Category): Promise<Category> => {
+  const result = await CategoriesModel.create(payload);
+  return result;
+};
+
+export const findAll = async (
+  query: any,
+  limit: number = 10,
+  page: number = 1
+): Promise<Category[]> => {
+  const result = await CategoriesModel.find(query)
+    .limit(limit)
+    .skip((page - 1) * limit)
+    .sort({ createdAt: -1 });
+  return result;
+};
+export const findOne = async (id: string): Promise<Category | null> => {
+  const result = await CategoriesModel.findById(id);
+  return result;
+};
+export const update = async (
+  id: string,
+  payload: Category
+): Promise<Category | null> => {
+  const result = await CategoriesModel.findOneAndUpdate({ _id: id }, payload, {
+    new: true,
+  });
+  return result;
+};
+export const remove = async (id: string): Promise<Category | null> => {
+  const result = await CategoriesModel.findOneAndDelete({
+    _id: id,
+  });
+  return result;
+};
