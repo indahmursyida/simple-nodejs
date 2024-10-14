a74324f7069ea8d0daa6b1aca9655719f0475f1a
indahmursyida <indahbahrina@gmail.com>
2024-10-14 12:57:25 +0700

    Added tugas API auth

--- tugas-service-pattern/src/routes/api.ts
+++ tugas-service-pattern/src/routes/api.ts
@@ -6,13 +6,14 @@ import productsController from "../controllers/products.controller";
 import categoriesController from "../controllers/categories.controller";
 import authController from "../controllers/auth.controller";
 import authMiddleware from "../middlewares/auth.middleware";
+import rbacMiddleware from "../middlewares/rbac.middleware";
 
 const router = express.Router();
 
 // Auth
 router.post("/auth/login", authController.login);
 router.post("/auth/register", authController.register);
-router.get("/auth/me", authMiddleware, authController.me);
+router.get("/auth/me",[authMiddleware, rbacMiddleware(["admin"])], authController.me);
 router.put("/auth/update-profile", authMiddleware, authController.updateProfile);
 // CRUD Categories
 router.get("/categories", categoriesController.findAll);
